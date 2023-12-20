78f44bf82119273f080da2a17f1925bde77fb3a0
Reis567 <matheusdosreis9@gmail.com>
2023-12-20 07:37:53 -0300

    initial commit

---
+++ src/components/Header/Header.styles.ts
@@ -0,0 +1,9 @@
+import styled from 'styled-components'
+
+
+export const Container = styled.div`
+  width: 100%;
+  background: #341d08;
+  display: flex;
+  
+`;
---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Header: React.FC = () => {
+  return (
+    <>
+    <h3>
+        Matheus dos Reis
+    </h3>
+    <nav>
+        <a href="#">Inicio</a>
+        <a href="#">Sobre</a>
+        <a href="#">Projetos</a>
+        <a href="#">Contatos</a>
+    </nav>
+    </>
+  );
+};
+
+export default Header;
