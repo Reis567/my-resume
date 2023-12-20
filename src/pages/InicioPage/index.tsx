ed99ee0f38abbe60407d228d69e89c08a3a15985
Reis567 <matheusdosreis9@gmail.com>
2023-12-20 19:00:47 -0300

    buttons to dowload resumes

--- src/pages/InicioPage/index.tsx
+++ src/pages/InicioPage/index.tsx
@@ -1,9 +1,27 @@
 import React from 'react';
 
+const curriculoPdfUrl = '/curriculos/curriculo.pdf';
+const resumePdfUrl = '/curriculos/resume.pdf';
+
 const Inicio: React.FC = () => {
+  const handleDownloadCurriculo = (pdfUrl: string, fileName: string) => {
+    const link = document.createElement('a');
+    link.href = pdfUrl;
+    link.setAttribute('download', fileName);
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+  };
+
   return (
     <div>
-      <h2>Matheus dos reis</h2>
+      <h2>Matheus dos Reis</h2>
+      <button onClick={() => handleDownloadCurriculo(curriculoPdfUrl, 'curriculo.pdf')}>
+        Baixar Currículo
+      </button>
+      <button onClick={() => handleDownloadCurriculo(resumePdfUrl, 'resume.pdf')}>
+        Download Resume
+      </button>
     </div>
   );
 };
