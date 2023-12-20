df409f061bbd6e5f7d44e9fa8be462b8d397a23a
Reis567 <matheusdosreis9@gmail.com>
2023-12-20 07:54:55 -0300

    useref type def

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,17 +1,33 @@
 import React from 'react';
+import { useRef } from 'react';
+import {FaBars, FaTimes} from 'react-icons/fa'
 
 const Header: React.FC = () => {
+    const navRef = useRef<HTMLDivElement | null>(null);
+    
+    const showNavBar = () =>{
+        navRef.current.classList.toggle("responsive_nav")
+    }
+
+
   return (
     <>
     <h3>
         Matheus dos Reis
     </h3>
-    <nav>
+    <nav ref={navRef}>
         <a href="#">Inicio</a>
         <a href="#">Sobre</a>
         <a href="#">Projetos</a>
         <a href="#">Contatos</a>
+        <button>
+            <FaTimes/>
+        </button>
     </nav>
+
+    <button>
+            <FaBars/>
+    </button>
     </>
   );
 };
