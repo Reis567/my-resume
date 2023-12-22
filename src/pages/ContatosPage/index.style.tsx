76eed91b4adbd3fb086b38f0cbcccff7163bbcd1
Reis567 <matheusdosreis9@gmail.com>
2023-12-22 17:57:26 -0300

    full contact page

--- src/pages/ContatosPage/index.style.tsx
+++ src/pages/ContatosPage/index.style.tsx
@@ -2,9 +2,41 @@ import styled from "styled-components";
 
 export const SContatos = styled.div`
     min-height: 100vh;
+    text-align:center;
 `
 
 export const STitle = styled.h2`
     font-size : 90px;
   font-weight: bolder;
+`
+
+export const SLinks = styled.div`
+text-align: center;
+display: flex;
+flex-direction: column;
+justify-content: center;
+align-items: center;
+gap: 30px;
+
+`
+export const LinkIcon = styled.div`
+padding: 15px;
+background-color:#341d08;
+border-radius:90px;
+color:#f7f2ee;
+  svg{
+    font-size: 50px;
+  }
+`
+export const LinkUrl = styled.a`
+font-size: 30px;
+padding: 15px;
+background: #341d08;
+border-radius:20px;
+color:#f7f2ee;
+margin-inline: 10px;
+`
+
+export const LinkDiv = styled.div`
+display: flex;
 `
--- src/pages/ContatosPage/index.tsx
+++ src/pages/ContatosPage/index.tsx
@@ -1,10 +1,53 @@
 import React from 'react';
-import { SContatos, STitle } from './index.style';
+import { SContatos, SLinks, STitle,LinkIcon, LinkUrl, LinkDiv } from './index.style';
+import { FaGithub, FaCodepen, FaLinkedin, FaEnvelope } from 'react-icons/fa';
 
 const Contatos: React.FC = () => {
   return (
     <SContatos>
       <STitle>Contatos</STitle>
+    <SLinks>
+      <LinkDiv>
+      
+      <LinkIcon>
+        <FaLinkedin/>
+      </LinkIcon>
+          <LinkUrl target='_blank' href="https://www.linkedin.com/in/matheus-dos-reis-08b74b1a4/7">
+          linkedin.com/in/matheus-dos-reis-08b74b1a4/
+          </LinkUrl>
+      </LinkDiv>
+
+      <LinkDiv>
+      
+      <LinkIcon>
+        <FaEnvelope/>
+      </LinkIcon>
+          <LinkUrl target='_blank'  href="mailto:matheusdosreis9@gmail.com">
+          matheusdosreis9@gmail.com
+          </LinkUrl>
+      </LinkDiv>
+
+      <LinkDiv>
+      
+      <LinkIcon>
+        <FaGithub/>
+      </LinkIcon>
+          <LinkUrl target='_blank'  href="https://github.com/Reis567">
+            github.com/Reis567
+          </LinkUrl>
+      </LinkDiv>
+
+      <LinkDiv>
+      
+      <LinkIcon>
+        <FaCodepen/>
+      </LinkIcon>
+          <LinkUrl target='_blank'  href="https://codepen.io/reis567">
+          codepen.io/reis567
+          </LinkUrl>
+      </LinkDiv>
+
+    </SLinks>
     </SContatos>
   );
 };
