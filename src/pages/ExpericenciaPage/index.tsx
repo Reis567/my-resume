4886dfb31072d87ef98e0a462cc2633ad1b8f24a
Reis567 <matheusdosreis9@gmail.com>
2024-04-12 10:12:11 -0300

    Fix : verbal time in exp page

--- src/pages/ExpericenciaPage/index.tsx
+++ src/pages/ExpericenciaPage/index.tsx
@@ -15,22 +15,22 @@ const Experiencia: React.FC = () => {
         </SubTitleS>
             <ParagrafS>
             
-            Fui responsável pela criação e manutenção de sistemas usando o framework Django. Isso incluiu a concepção, desenvolvimento e implementação de aplicativos web robustos e escaláveis
+            Responsável pela criação e manutenção de sistemas usando o framework Django. Isso incluiu a concepção, desenvolvimento e implementação de aplicativos web robustos e escaláveis
             </ParagrafS>
             <ParagrafS>
-            Desempenhei a manutenção e administração de servidores Linux, garantindo sua estabilidade e segurança. Isso envolveu a configuração, monitoramento e resolução de problemas para garantir a operação contínua dos sistemas.
-            </ParagrafS>
-
-            <ParagrafS>
-            Realizei instalações completas de máquinas Linux, desde a configuração inicial do sistema operacional até a configuração avançada do servidor, incluindo a implementação de certificados HTTPS para garantir a segurança das comunicações
+            Manutenção e administração de servidores Linux, garantindo sua estabilidade e segurança. Envolvendo configuração, monitoramento e resolução de problemas para garantir a operação contínua dos sistemas.
             </ParagrafS>
 
             <ParagrafS>
-            Utilizei o Python para realizar webscraping, coletands dados de várias fontes da web. Isso envolveu a escrita de scripts personalizados para extrair informações relevantes de diferentes sites de forma automatizada.
+            Instalações completas de máquinas Linux, desde a configuração inicial do sistema operacional até a configuração avançada do servidor, incluindo a implementação de certificados HTTPS para garantir a segurança das comunicações
             </ParagrafS>
 
             <ParagrafS>
-            Fui encarregado do desenvolvimento e manutenção de scripts em Python para automatizar e melhorar a eficiência operacional.
+            Utilização o Python para realizar webscraping, coletando dados de várias fontes da web. Envolve a escrita de scripts personalizados para extrair informações relevantes de diferentes sites de forma automatizada.
+            </ParagrafS>
+
+            <ParagrafS>
+            Encarregado do desenvolvimento e manutenção de scripts em Python para automatizar e melhorar a eficiência operacional.
             </ParagrafS>
 
 
